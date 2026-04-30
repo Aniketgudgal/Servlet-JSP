dcdcd2e82e628b2d0abc86ecdb1bd3fff4fb46d6
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-04-30 13:21:19 +0530

    Servlet Mapping using XML File Configuration

---
+++ LoginValidation/src/main/java/com/LoginServlet/Register.java
@@ -0,0 +1,28 @@
+package com.LoginServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//@WebServlet("/register")
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// String methodName = request.getMethod();
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1> Hello world" + "</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
