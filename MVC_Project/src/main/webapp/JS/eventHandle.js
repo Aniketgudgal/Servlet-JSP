0ff971725855931f04f6bb77477a3b4170581b8f
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-05-06 23:49:36 +0530

    Search Data using Ajax

---
+++ MVC_Project/src/main/webapp/JS/eventHandle.js
@@ -0,0 +1,52 @@
+function searchEmployee()
+{
+	alert('Hello word');
+}
+function deleteAlert()
+{
+	alert('Are you sure ?');
+}
+function confirmDelete() {
+
+    return confirm("Do you want to delete this record?");
+}
+function updateConfirm()
+{
+	return confirm("Are you sure to Update?");
+}
+function addDeptConfirm()
+{
+	return confirm("Sure to Add?");
+}
+function confirmToDelDepartment()
+{
+	return confirm("Are You Sure to Delete Department?");
+}
+function sureToUpdateDept()
+{
+	return confirm("Sure to Update Information");
+}
+function confirmToAddEmp()
+{
+	return confirm("Confirm to Add Employee");
+}
+function accessDataEmp() {
+
+	let data = document.getElementById("data");
+	let val = data.value;
+	console.log(val);
+	let tbody = document.getElementById("tbody");
+	tbody.innerHTML = "";
+    let aj = new XMLHttpRequest();
+    aj.open("GET", "/MVC_Project/searchBEmp?data="+val, true);
+
+    aj.onreadystatechange = function() {
+
+        if(this.readyState == 4 && this.status == 200) {
+
+		tbody.innerHTML = this.responseText;	
+        }
+    };
+
+    aj.send();
+}
